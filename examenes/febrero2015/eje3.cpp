8b065b412dff416392f6b746d531f03385349203
Alexis Cumbal <genericmail0001@gmail.com>
2017-02-08 20:47:41 +0100

    eje3 a medias

--- examenes/febrero2015/eje3.cpp
+++ examenes/febrero2015/eje3.cpp
@@ -1,23 +1,87 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 #define MAX 10000
+#define MP 20
+#define QP 20
+
 
 struct Producto {
     int pre;
     int prot;
     int cal;
 };
+bool esValida(){
+    return true;
+}
+bool esSolucion(int maxprecio, int maxproteinas, int mincal,
+                    int sumprecio, int sumprote, int sumcal){
+    //si no supera maxprecio, y no supera maxprot, y es < que minmincal
+    return (sumprecio < maxprecio) && 
+                (sumprote < maxproteinas) && 
+                      (sumcal < mincal);
+}
+void sumarDatos(Producto sol[], int tam, int &sumpre, int &sumprot, int &sumcal){
+    
+    int i = 0;
+    while(i<=tam){
+        Producto prod;
+        prod = sol[i];
+        sumpre += prod.pre;
+        sumprot += prod.prot;
+        sumcal += prod.cal;
+        i++;
+    }
+}
+
+/*
+solve(prods,numcasos,sol,mejorSol, MP, QP);
+*/
+void solve(Producto prods[], int N, Producto sol[], Producto mejorSol[],int maxprecio, int maxproteinas, int &minCal, int nivel){
+    int i = 0;
+    while(i < N){
+        sol[i] = prods[i];
+        if(esValida()){
+            cout << "Es valida"<< endl; 
+            int sumpre = 0, sumprot = 0, sumcal = 0;
+            sumarDatos(sol,i,sumpre,sumprot,sumcal);
+            /*(int maxprecio, int maxproteinas, int mincal,
+                    int sumprecio, int sumprote, int sumcal)*/
+            if(esSolucion(maxprecio, maxproteinas, minCal, sumpre,sumprot, sumcal)){
+                cout << "esSolucion"<< endl;
+                mejorSol[i] = sol[i];
+                /*cout << "esSolucion"<< endl;
+                int j = 0;
+                while(j < N ){
+                    mejorSol[j] = sol[j];
+                    j++;
+                }
+                minCal = sumcal;*/
+            }else{
+
+            }
+        }
+        i++;
+    }
+}
+
+
 //minimizar el número de calorías
 void printProds(Producto prods[], int numprods){
     int i=0;
+     cout << "PRODUCTOS "<<endl;
     while(i < numprods){
         Producto prod = prods[i];
-        cout << "Precio: "<< endl;
-        cin >> prod.precio;
-        cout << "Proteinas: "<< endl;
-        cin >> prod.prot;
-        cout << "Precio: "<< endl;
-        cin >> prod.cal;
+        cout << "Precio: ";
+        cout << prod.pre;
+        cout << endl;
+        cout << "Proteinas: ";
+        cout << prod.prot;
+        cout << endl;
+        cout << "Calorias: ";
+        cout << prod.cal;
+        cout << endl;
+        cout << endl;
         i++;
     }
 }
@@ -28,19 +92,22 @@ int main(int arg, char **argv){
     cout << "Numero de productos de la lista"<< endl;
     cin >> numcasos;
     Producto prods[MAX];
-    while(numcasos >= 0){
+    int i = 0;
+    while(i < numcasos){
         Producto prod;
         cout << "Precio: "<< endl;
-        cin >> prod.precio;
+        cin >> prod.pre;
         cout << "Proteinas: "<< endl;
         cin >> prod.prot;
-        cout << "Precio: "<< endl;
+        cout << "Calorias: "<< endl;
         cin >> prod.cal;
-        prods[numcasos] =  prod;
-        numcasos--;
+        prods[i] =  prod;
+        i++;
     }
-
-
+    Producto sol[MAX], mejorSol[MAX];
+    int minCal = INT_MAX;
+    solve(prods,numcasos,sol,mejorSol, MP, QP, minCal,0);
+    printProds(mejorSol,numcasos);
     char answer = 'd'; // new answer variable
 	cout << "Press any key to continue, not enter";
 	cin >> answer;
