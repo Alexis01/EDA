74bc62e343ca217f24a32e347d94d01f08415090
Usuario Local <Usuario Local>
2017-03-24 16:39:04 +0100

    estructura

---
+++ 2cuatri/practicas/week4/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include "List.h"
+#include <cstdlib>
+
+using namespace std;
+
+
+void mostrarLista(List<int>* l){
+	List<int>::ConstIterator it = l->cbegin();
+	if (it != l->cend()){
+		cout << it.elem();
+		it.next();
+	}
+	while (it != l->cend()){
+		cout << " " << it.elem();
+		it.next();
+	}
+	cout << endl;
+}
+
+
+
+void main(){
+	int n, numCasos, elem;
+	cin >> numCasos;
+	for (int i = 0; i < numCasos; i++){
+		List<int>* l = new List<int>();
+		cin >> n;
+		while (n != -1){
+			l->push_back(n);
+			cin >> n;
+		}
+		cin >> elem;
+		mostrarLista(l);
+		//l->removeAll(elem);
+		mostrarLista(l);
+		delete l;
+	}
+}
