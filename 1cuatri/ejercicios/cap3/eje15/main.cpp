157b825c38a50c1078735da266fec580468a8f32
Alexis Cumbal <genericmail0001@gmail.com>
2017-10-04 12:56:42 +0200

    eje 15 ok

--- 1cuatri/ejercicios/cap3/eje15/main.cpp
+++ 1cuatri/ejercicios/cap3/eje15/main.cpp
@@ -1,8 +1,9 @@
 /*
-{N > 0}
-    fun pares( int V[N]) return int x
-{x = (#i : 0 ≤ i < N : V [i]mod 2 = 0)}
-X es igual al productorio de todos los elementos en posiciones pares
+Deriva un algoritmo que satisfaga la siguiente especicación:
+    {N > 0}
+        fun suma-buenos( int X[N]) return int s
+    {s = (Σi : 0 ≤ i < N ∧ bueno(i, X) : X[i])}
+donde bueno(i, X) ≡ (X[i] = 2 i ). No se pueden emplear operaciones que calculen potencias.
 */
 #include <iostream>
 #include <fstream>
@@ -17,34 +18,34 @@ auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin t
 #endif
 
 
-int solve(int V[],int N ){
-    int prod = 1, n = 0;
+int solve(int v[],int N ){
+    int sum = 0, n = 0;
     while(n < N){
-        if(n % 2 == 0 ){
-            prod *= V[n];
+        if(v[n] == pow(2,n) ){
+            sum += v[n];
         }
         n++;
     }
-    return prod;
+    return sum;
 }
 
-int solveRecursiveGen(int v[], int i, int tam, int &prodPares){
-    if( (i == tam) && (i%2==0) ){
-        prodPares *= v[i];
+int solveRecursiveGen(int v[], int i, int tam, int &sumBuenos){
+    if( (i == tam) && (v[i] == pow(2,i)) ){
+        sumBuenos += v[i];
     }else{
         if(i <= tam){
-            if(i%2 == 0){
-                prodPares *= v[i];
+            if(v[i] == pow(2,i)){
+                sumBuenos += v[i];
             }
-            solveRecursiveGen(v,i+1,tam,prodPares);
+            solveRecursiveGen(v,i+1,tam,sumBuenos);
         }
     }
 }
 
 int solveRecursive(int V[], int n ){
-    int prodPares = 1;
-    solveRecursiveGen(V,0,n-1,prodPares);
-    return prodPares;
+    int sumBuenos = 0;
+    solveRecursiveGen(V,0,n-1,sumBuenos);
+    return sumBuenos;
 }
 
 int main(){
@@ -58,7 +59,6 @@ int main(){
             cin >> V[posV];
             posV++;
         }
-        
         cout << "Iterative: ";
         cout << solve( V,posV ) << endl;
         cout << "Recursive: ";
