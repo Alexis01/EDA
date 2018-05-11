7c9316689104a1cf26f5f7da9d214b6cd2381204
Usuario Local <Usuario Local>
2018-05-11 17:04:58 +0200

    fasdf

--- 2cuatri/practicas/week9/main.cpp
+++ 2cuatri/practicas/week9/main.cpp
@@ -3,6 +3,7 @@
 #include "TreeMap.h"
 #include <string>
 #include "List.h"
+#include <fstream>
 
 using namespace std;
 
@@ -20,7 +21,18 @@ void leerDiccionario(Diccionario& diccionario){
 		cin.get(c);
 	}
 }
-
+void imprimeLista(string simbol, List<string> aImprimir){
+	if (!aImprimir.empty()){
+		cout << simbol << " ";
+		int pos = 0;
+		while (pos < aImprimir.size() - 1){
+			cout << aImprimir.at(pos) << " ";
+			pos++;
+		}
+		cout << aImprimir.at(pos) << endl;
+	}
+	
+}
 
 void resuelveCaso(){
 	Diccionario antiguo;
@@ -32,23 +44,49 @@ void resuelveCaso(){
     List<string> add;
     Diccionario::Iterator itviejo = antiguo.begin();
     Diccionario::Iterator itnuevo = nuevo.begin();
-    while(itviejo != antiguo.end() ){
-        if( itviejo.value() < itnuevo.value() ){
-            del.push_back(itviejo.value());
+	while (itviejo != antiguo.end() && itnuevo != nuevo.end()){
+        if( itviejo.key() < itnuevo.key() ){
+            del.push_back(itviejo.key());
             itviejo.next();
-        }
-        if( itviejo.value() == itnuevo.value()){
-            if( itviejo.key() != itnuevo.key()){
-                mod.push_back(itviejo.value());
-            }
-            itviejo.next();
-            itnuevo.next();
-        }
-        if( itviejo.value() > itnuevo.value() ){
-            add.push_back(itviejo.value());
-            itnuevo.next();
-        } 
+		}else {
+			if (itviejo.key() == itnuevo.key()){
+				if (itviejo.value() != itnuevo.value()){
+					mod.push_back(itviejo.key());
+				}
+				itviejo.next();
+				itnuevo.next();
+			}else{
+				if (itviejo.key() > itnuevo.key()){
+					add.push_back(itnuevo.key());
+					itnuevo.next();
+				}
+			}
+		}
     }
+
+	if (itviejo != antiguo.end()){
+		while (itviejo != antiguo.end()){
+			del.push_back(itviejo.key());
+			itviejo.next();
+		}
+	}
+	if (itnuevo != nuevo.end()){
+		while (itnuevo != nuevo.end()){
+			add.push_back(itnuevo.key());
+			itnuevo.next();
+
+		}
+	}
+	if (add.empty() && del.empty() && mod.empty()){
+		cout << "Sin cambios" << endl;
+	}
+	else{
+		imprimeLista("+", add);
+		imprimeLista("-", del);
+		imprimeLista("*", mod);
+	}
+	
+	cout << "----" << endl;
 }
 
 int main(){
